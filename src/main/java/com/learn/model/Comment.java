5a58c5959cc876ac8214e794e3f880d5202a4825
m3mpm <zgeorgan@cl-b5.nsk.21-school.ru>
2023-11-15 14:43:45 +0700

    add model to project

---
+++ src/main/java/com/learn/model/Comment.java
@@ -0,0 +1,25 @@
+package com.learn.model;
+
+public class Comment {
+    private  String author;
+    private String text;
+
+    public Comment() {
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
