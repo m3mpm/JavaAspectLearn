ed172826f6acd6b5d91c1cc28bb714423a1f3a09
m3mpm <zgeorgan@cl-b5.nsk.21-school.ru>
2023-11-15 14:49:24 +0700

    add services

---
+++ src/main/java/com/learn/services/CommentService.java
@@ -0,0 +1,17 @@
+package com.learn.services;
+
+import com.learn.annotation.ToLog;
+import com.learn.model.Comment;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+@Service
+public class CommentService {
+
+    private Logger logger = Logger.getLogger(CommentService.class.getName());
+
+    public void publishComment(Comment comment){
+        logger.info("Publishing comment:" + comment.getText());
+    }
+}
