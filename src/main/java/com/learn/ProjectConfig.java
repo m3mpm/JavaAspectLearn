7e9222f166bcf4b010929b1c5fbcd7ccab36500e
m3mpm <m3mpm@yandex.ru>
2023-11-15 14:56:07 +0700

    add ProjectConfig.java file

---
+++ src/main/java/com/learn/ProjectConfig.java
@@ -0,0 +1,10 @@
+package com.learn;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan(basePackages = "com.learn.services")
+public class ProjectConfig {}
